bb411b62d4f9914e622650ef5d505197fef2f30b
TED-PC\USER <craig@gamevault.co.za>
2025-08-17 18:50:18 +0200

    Update to startup and project structure

---
+++ DB2File/DB2File/DB2FileService/Extensions/AddApplicationServices.cs
@@ -0,0 +1,13 @@
+﻿namespace DB2FileService.Extensions;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddApplicationServices(this IServiceCollection services,
+                                                            IConfiguration configuration)
+    {
+        services.AddEndpointsApiExplorer();
+        services.AddSwaggerGen();
+        
+        return services;
+    }
+}
---
+++ DB2File/DB2File/DB2FileService/Extensions/DataServicesExtension.cs
@@ -0,0 +1,13 @@
+﻿namespace DB2FileService.Extensions;
+
+public static class DataServicesExtension
+{
+    public static IServiceCollection AddDataServices(this IServiceCollection services,
+                                                     IConfiguration configuration,
+                                                     IWebHostEnvironment environment)
+    {
+
+
+        return services;
+    }
+}
